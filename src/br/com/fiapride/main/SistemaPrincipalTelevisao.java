478b950118aacda7380f31b58c73e0ee6102201b
Rezenderzd <gmrezende07@gmail.com>
2026-03-03 22:32:56 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/main/SistemaPrincipalTelevisao.java
+++ src/br/com/fiapride/main/SistemaPrincipalTelevisao.java
@@ -5,20 +5,26 @@ import br.com.fiapride.model.Televisao;
 public class SistemaPrincipalTelevisao {
 
 	public static void main(String[] args) {
-		Televisao televisao1 = new Televisao();
-		televisao1.modelo = "Tubo";
-		televisao1.polegadas = 32;
-		televisao1.tipoImagem = "4K";
-
-				
-		Televisao televisao2 = new Televisao();
-		televisao2.modelo="Plana";
-		televisao2.polegadas = 46;
-		televisao2.tipoImagem = "Full HD";
+		// INSTANCIAÇÃO
+        // O comando 'new' aloca memória para um novo objeto.
+        // Criando o primeiro passageiro (Objeto 1)
+		Televisao televisao1 = new Televisao (32, "Tubo", "4K", 39, 70);
 		
+		// Criando o segundo passageiro (Objeto 2)		
+		Televisao televisao2 = new Televisao(46, "Plana", "Full HD", 71, 30);
+		
+		// Exibindo os dados no Console
 		System.out.println("---Televisões disponíveis---");
-		System.out.println("Televisão 1 "+ televisao1.modelo +"|"+ televisao1.polegadas +" polegadas|" + televisao1.tipoImagem);
-		System.out.println("Televisão 2 "+ televisao2.modelo +"|" + televisao2.polegadas +" polegadas|" + televisao2.tipoImagem);
+		System.out.println("Televisão 1 "+ televisao1.modelo +"|"+ televisao1.polegadas +" polegadas|" + televisao1.tipoImagem +"Canal: "+televisao1.canalAtual +"| Volume Atual: "+televisao1.volumeAtual);
+		System.out.println("Televisão 2 "+ televisao2.modelo +"|" + televisao2.polegadas +" polegadas|" + televisao2.tipoImagem +"Canal: "+televisao2.canalAtual +"| Volume Atual: "+televisao2.volumeAtual);
+		// Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
+		
+		televisao2.mudarCanal(televisao2.canalAtual, 10);
+		televisao1.mudarVolume(televisao1.volumeAtual);
+		
+		System.out.println("---Televisões pós método---");
+		System.out.println("Televisão 1 "+ televisao1.modelo +"|"+ televisao1.polegadas +" polegadas|" + televisao1.tipoImagem +" Canal: "+televisao1.canalAtual +"| Volume Atual: "+televisao1.volumeAtual);
+		System.out.println("Televisão 2 "+ televisao2.modelo +"|" + televisao2.polegadas +" polegadas|" + televisao2.tipoImagem +" Canal: "+televisao2.canalAtual +"| Volume Atual: "+televisao2.volumeAtual);
 	}
 
 }
--- src/br/com/fiapride/model/Televisao.java
+++ src/br/com/fiapride/model/Televisao.java
@@ -1,7 +1,35 @@
 package br.com.fiapride.model;
 
+//A Classe define a estrutura. Ela não é o passageiro real, é apenas o modelo
 public class Televisao {
+	// Atributos (Características)
 	public int polegadas;
 	public String modelo;
 	public String tipoImagem;
+	public int canalAtual;
+	public int volumeAtual;
+	
+	public
+	Televisao(int polegadas, String modelo, String tipoImagem, int canalAtual, int volumeAtual) {
+        this.polegadas = polegadas;
+        this.modelo = modelo; // Novo Atributo
+        this.tipoImagem = tipoImagem;
+        this.canalAtual = canalAtual;
+        this.volumeAtual = volumeAtual;
+    }
+	
+	public void mudarVolume(int volumeAtual ) {
+		if(volumeAtual>50) {
+			this.volumeAtual = volumeAtual/2;
+			System.out.println("Volume atual é: "+ volumeAtual);
+			return;
+		}
+	}
+	public void mudarCanal(int quantidadeMudarCanal, int canalAtual) {
+		this.canalAtual = canalAtual + quantidadeMudarCanal;
+		System.out.println("O canal atual é:"+ canalAtual);
+		return;
+	}
+    // Nota: Por enquanto usamos 'public' para facilitar o aprendizado.
+    // Nas próximas aulas, aprenderemos a proteger esses dados.
 }
