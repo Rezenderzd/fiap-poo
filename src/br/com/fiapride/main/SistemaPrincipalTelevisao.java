b78b520a04d065e2b378c10cfc419c251bd29fdf
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-10 22:34:35 -0300

    refactor: aplica encapsulamento na classe Passageiro e Objeto Pessoal

--- src/br/com/fiapride/main/SistemaPrincipalTelevisao.java
+++ src/br/com/fiapride/main/SistemaPrincipalTelevisao.java
@@ -3,28 +3,27 @@ package br.com.fiapride.main;
 import br.com.fiapride.model.Televisao;
 
 public class SistemaPrincipalTelevisao {
-
 	public static void main(String[] args) {
 		// INSTANCIAÇÃO
         // O comando 'new' aloca memória para um novo objeto.
         // Criando o primeiro passageiro (Objeto 1)
-		Televisao televisao1 = new Televisao (32, "Tubo", "4K", 39, 70);
+		Televisao televisao1 = new Televisao (32, "Tubo", "4K", 39, 110);
 		
 		// Criando o segundo passageiro (Objeto 2)		
-		Televisao televisao2 = new Televisao(46, "Plana", "Full HD", 71, 30);
+		Televisao televisao2 = new Televisao(46, "Plana", "Full HD", 71, -20);
 		
 		// Exibindo os dados no Console
 		System.out.println("---Televisões disponíveis---");
-		System.out.println("Televisão 1 "+ televisao1.modelo +"|"+ televisao1.polegadas +" polegadas|" + televisao1.tipoImagem +"Canal: "+televisao1.canalAtual +"| Volume Atual: "+televisao1.volumeAtual);
-		System.out.println("Televisão 2 "+ televisao2.modelo +"|" + televisao2.polegadas +" polegadas|" + televisao2.tipoImagem +"Canal: "+televisao2.canalAtual +"| Volume Atual: "+televisao2.volumeAtual);
+		System.out.println("Televisão 1 "+ televisao1.getModelo() +"|"+ televisao1.getPolegadas() +" polegadas|" + televisao1.getTipoImagem() +"Canal: "+televisao1.getCanalAtual() +"| Volume Atual: "+televisao1.getVolumeAtual());
+		System.out.println("Televisão 2 "+ televisao2.getModelo() +"|" + televisao2.getPolegadas() +" polegadas|" + televisao2.getTipoImagem() +"Canal: "+televisao2.getCanalAtual() +"| Volume Atual: "+televisao2.getVolumeAtual());
 		// Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
 		
-		televisao2.mudarCanal(televisao2.canalAtual, 10);
-		televisao1.mudarVolume(televisao1.volumeAtual);
+		televisao2.mudarCanal(televisao2.getCanalAtual(), 10);
+		televisao1.mudarVolume(televisao1.getVolumeAtual());
 		
 		System.out.println("---Televisões pós método---");
-		System.out.println("Televisão 1 "+ televisao1.modelo +"|"+ televisao1.polegadas +" polegadas|" + televisao1.tipoImagem +" Canal: "+televisao1.canalAtual +"| Volume Atual: "+televisao1.volumeAtual);
-		System.out.println("Televisão 2 "+ televisao2.modelo +"|" + televisao2.polegadas +" polegadas|" + televisao2.tipoImagem +" Canal: "+televisao2.canalAtual +"| Volume Atual: "+televisao2.volumeAtual);
+		System.out.println("Televisão 1 "+ televisao1.getModelo() +"|"+ televisao1.getPolegadas() +" polegadas|" + televisao1.getTipoImagem() +"Canal: "+televisao1.getCanalAtual() +"| Volume Atual: "+televisao1.getVolumeAtual());
+		System.out.println("Televisão 2 "+ televisao2.getModelo() +"|" + televisao2.getPolegadas() +" polegadas|" + televisao2.getTipoImagem() +"Canal: "+televisao2.getCanalAtual() +"| Volume Atual: "+televisao2.getVolumeAtual());
 	}
 
 }
--- src/br/com/fiapride/model/Televisao.java
+++ src/br/com/fiapride/model/Televisao.java
@@ -3,19 +3,18 @@ package br.com.fiapride.model;
 //A Classe define a estrutura. Ela não é o passageiro real, é apenas o modelo
 public class Televisao {
 	// Atributos (Características)
-	public int polegadas;
-	public String modelo;
-	public String tipoImagem;
-	public int canalAtual;
-	public int volumeAtual;
+	private int polegadas;
+	private String modelo;
+	private String tipoImagem;
+	private int canalAtual;
+	private int volumeAtual;
 	
-	public
-	Televisao(int polegadas, String modelo, String tipoImagem, int canalAtual, int volumeAtual) {
-        this.polegadas = polegadas;
-        this.modelo = modelo; // Novo Atributo
-        this.tipoImagem = tipoImagem;
-        this.canalAtual = canalAtual;
-        this.volumeAtual = volumeAtual;
+	public Televisao(int polegadas, String modelo, String tipoImagem, int canalAtual, int volumeAtual) {
+        this.setPolegadas(polegadas);
+        this.setModelo(modelo);
+        this.setTipoImagem(tipoImagem);
+        this.setCanalAtual(canalAtual);
+        this.setVolumeAtual(volumeAtual);
     }
 	
 	public void mudarVolume(int volumeAtual ) {
@@ -30,6 +29,54 @@ public class Televisao {
 		System.out.println("O canal atual é:"+ canalAtual);
 		return;
 	}
-    // Nota: Por enquanto usamos 'public' para facilitar o aprendizado.
+
+	public int getPolegadas() {
+		return polegadas;
+	}
+
+	private void setPolegadas(int polegadas) {
+		this.polegadas = polegadas;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	private void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getTipoImagem() {
+		return tipoImagem;
+	}
+
+	private void setTipoImagem(String tipoImagem) {
+		this.tipoImagem = tipoImagem;
+	}
+
+	public int getCanalAtual() {
+		return canalAtual;
+	}
+
+	private void setCanalAtual(int canalAtual) {
+		this.canalAtual = canalAtual;
+	}
+
+	public int getVolumeAtual() {
+		return volumeAtual;
+	}
+
+	private void setVolumeAtual(int volumeAtual) {
+		if(volumeAtual>100) {
+			this.volumeAtual = 100;
+			return;
+		}else if(volumeAtual<0) {
+			this.volumeAtual = 0;
+			return;
+		}
+		this.volumeAtual = volumeAtual;
+	}
+
+	// Nota: Por enquanto usamos 'public' para facilitar o aprendizado.
     // Nas próximas aulas, aprenderemos a proteger esses dados.
 }
